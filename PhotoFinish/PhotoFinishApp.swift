406945cd57d76ca0a2a5bdb1aab4ba3af53adcc1
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-09-19 13:42:47 +0200

    Initial Commit

---
+++ PhotoFinish/PhotoFinishApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoFinishApp.swift
+//  PhotoFinish
+//
+//  Created by Eric on 19/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct PhotoFinishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
